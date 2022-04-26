6a1d47a074527fadb1580f0f8b71c548001d2d3f
MaciPaci <maciej.stachowiak420@gmail.com>
2022-04-27 00:26:53 +0200

    Added second file read and db insertion

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ CREATE TABLE unique_tracks(
 
 create_tracks_play_history_table = '''
 CREATE TABLE tracks_play_history(
-    user_id VARCHAR(255) PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
+    user_id VARCHAR(255),
     track_id VARCHAR(255),
     play_date TIMESTAMP
 )
@@ -54,11 +55,11 @@ def remove_tables(db_cursor):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--tracks", type=str, required=True)
-    # parser.add_argument("--triplets", type=str, required=True)
+    parser.add_argument("--triplets", type=str, required=True)
     args = parser.parse_args()
 
     tracks_path = args.tracks
-    # triplets_path = args.triplets
+    triplets_path = args.triplets
 
     with connect('track_library.db') as db_connector:
         db_cursor = db_connector.cursor()
@@ -66,11 +67,32 @@ def main():
         create_tables(db_cursor)
 
         with open(tracks_path, 'r', encoding='latin-1') as f:
+            c = 0
             for line in f:
-                print(line)
-                line = line.strip()
-                line = line.split('<SEP>')
-                print(line)
+                if c >= 100:
+                    break
+                tracks = line.strip().split('<SEP>')
+                print(tracks)
+                if len(tracks) == 4:
+                    db_cursor.execute('INSERT INTO unique_tracks VALUES (?, ?, ?, ?)', tracks)
+                c += 1
+        f.close()
+
+        with open(triplets_path, 'r', encoding='latin-1') as f:
+            c = 0
+            for line in f:
+                if c >= 100:
+                    break
+                triplets = line.strip().split('<SEP>')
+                print(triplets)
+                if len(triplets) == 3:
+                    db_cursor.execute('INSERT INTO tracks_play_history (user_id, track_id, play_date) VALUES (?, ?, ?)',
+                                      triplets)
+                c += 1
+        f.close()
+
+    for row in db_cursor.execute('SELECT * FROM tracks_play_history'):
+        print(row)
 
 
 if __name__ == '__main__':
